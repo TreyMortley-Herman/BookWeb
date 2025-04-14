64206a1b68eed7c4d9fc96535c6f8cd66c1ecb54
AmaniS999 . <amani.salazar.3e@gmail.com>
2025-04-14 06:36:05 +0000

    Added Firebase

---
+++ public/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBC6_5y0N4Rbn-S42tboZaq3ZzHaRpq_L0",
+    authDomain: "bookweb-36055.firebaseapp.com",
+    projectId: "bookweb-36055",
+    storageBucket: "bookweb-36055.firebasestorage.app",
+    messagingSenderId: "227791068443",
+    appId: "1:227791068443:web:1c819c673dcc234c8847be"
+  };
+  
+  export default firebaseConfig;
